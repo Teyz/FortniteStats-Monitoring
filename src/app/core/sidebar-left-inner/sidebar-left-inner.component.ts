a5788aedddc4299684aaa65507310f0d391e866d
Bastien <contact.teyz@gmail.com>
2019-10-28 21:06:17 +0100

    Add API

--- src/app/core/sidebar-left-inner/sidebar-left-inner.component.ts
+++ src/app/core/sidebar-left-inner/sidebar-left-inner.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../twitter/twitter.service';
 
 @Component({
   selector: 'app-sidebar-left-inner',
   templateUrl: './sidebar-left-inner.component.html'
 })
-export class SidebarLeftInnerComponent {}
+export class SidebarLeftInnerComponent implements OnInit {
+
+  public status = {};
+
+  constructor(
+    public FNRBapi: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.FNRBapi.getStatus().subscribe((data) => {
+      this.status = data;
+    });
+  }
+}

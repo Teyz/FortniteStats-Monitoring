19da60e86086378ddd7e5df3251e28024dcd296f
Bastien <contact.teyz@gmail.com>
2019-10-30 12:25:17 +0100

    Reorganization

--- src/app/admin-lte.conf.ts
+++ src/app/admin-lte.conf.ts
@@ -9,10 +9,10 @@ export const adminLteConf = {
   // isSidebarRightOverContent: true,
   // layout: 'normal',
   sidebarLeftMenu: [
+    {label: 'Website', separator: true},
+    {label: 'Bot - @FNBRStats', separator: true},
     {label: 'Status', route: 'status', iconClasses: 'fa fa-terminal'},
-    {label: 'MONITORING', separator: true},
     {label: 'Twitter', route: 'twitter', iconClasses: 'fa fa-twitter'},
-    {label: 'Statistics', separator: true},
     {label: 'Statistics', route: 'statistics', iconClasses: 'fa fa-globe'}
   ]
 };
